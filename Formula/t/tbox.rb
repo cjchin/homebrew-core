bdcd2baaaf2044ef9c78086296441ffd328add33
Sean Molenaar <SMillerDev@users.noreply.github.com>
2025-11-25 14:42:21 +0100

    Change head branch from master to dev

--- Formula/t/tbox.rb
+++ Formula/t/tbox.rb
@@ -4,7 +4,7 @@ class Tbox < Formula
   url "https://github.com/tboox/tbox/archive/refs/tags/v1.7.9.tar.gz"
   sha256 "8d4bba88bb279c4ff71677d15f8bfc20dfbdc3b4eee27b540fb979fe5af65e56"
   license "Apache-2.0"
-  head "https://github.com/tboox/tbox.git", branch: "master"
+  head "https://github.com/tboox/tbox.git", branch: "dev"
 
   livecheck do
     url :stable
